9ceea3ca9de92948aebeaa42f7955784c3355a54
Wolfcodes91 <Dave.M.Wolfberg@gmail.com>
2022-04-09 18:16:31 -0700

    more tests added

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 const chai = require('chai');
+const { it } = require('mocha');
 const assert = chai.assert;
 const findMax = require('../findMax');
 
@@ -10,6 +11,15 @@ describe("the findMax function", ()=>{
     it("should return the correct maximum", ()=>{
         assert.equal(findMax([1,3,2]), 3);
         assert.equal(findMax([-1,-2,-3]), -1);
+    })
+    it("should not return a string", ()=> {
+        assert.isNotString(findMax([1, 3, 2]), "not a string")
+    })
+    it("should not return NaN", ()=> {
+        assert.isNotNaN(findMax([1, 3, 2]), "not NaN")
+    })
+    it("should not return an Array", ()=> {
+        assert.isNotArray(findMax([1, 3, 2]), "not an Array")
     })
 });
 
